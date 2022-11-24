9bf890b699b2b0c375e4a22854c48ea471fd95c5
Quinten Leung <wleung105@my.bcit.ca>
2022-11-24 15:46:05 -0800

    Formatting

--- stale_file.py
+++ stale_file.py
@@ -1,29 +1,32 @@
 import datetime as DT
-import sys 
+import sys
 import os
 import pathlib as PL
 
-#? Current test input
+# ? Current test input
 #! python .\stale_file.py 'C:\Users\Curry\OneDrive\Desktop\Python Code\Weeek11\test_root'  2021-05-09 print
 
-def Print(Directory:PL.Path, Date:DT.datetime):
+
+def Print(Directory: PL.Path, Date: DT.datetime):
     """Takes in a directory and date. Prints all files in that directory and subdirectories that have not been modified since Date.
 
     Args:
         Directory (PL.Path): Path of the of the directory we want to print all files from.
         Date (DT.datetime): The date where we want to judge all files in the directory.
-    """    
+    """
     Print_Date = Date.strftime('%Y-%m-%d')
     print(f'\nRoot directory: {Directory}')
     print(f'Best Before Date: {Print_Date}')
     print(f'Files to delete:')
-    for file in ScrapeFolder(Directory,Date,Directory):
+    for file in ScrapeFolder(Directory, Date, Directory):
         print(f'{file[0]:{60}}  {file[1]}   {file[2]:{3}} B')
-        
+
+
 def Report():
     print('report')
-    
-def ScrapeFolder(Directory:PL.Path,Date:DT.datetime,Root:PL.Path) -> list[tuple[str,str,int]]:
+
+
+def ScrapeFolder(Directory: PL.Path, Date: DT.datetime, Root: PL.Path) -> list[tuple[str, str, int]]:
     """Takes in a Directory,Date,and Root directory and returns a list of files that was last modified before the date.
 
     Args:
@@ -33,24 +36,25 @@ def ScrapeFolder(Directory:PL.Path,Date:DT.datetime,Root:PL.Path) -> list[tuple[
 
     Returns:
         list[tuple[str,str,int]]: a List of a tuples containing the files that were last modified before the date specified.
-    """    
+    """
     Files = os.listdir(Directory)
     List_Files = []
     for file in Files:
-        path = os.path.join(Directory,file)
+        path = os.path.join(Directory, file)
         M_TIME = DT.datetime.fromtimestamp(os.path.getmtime(path))
         if Date > M_TIME:
             M_TIME = M_TIME.strftime('%Y-%m-%d')
             SIZE = os.path.getsize(path)
-            rel_file = os.path.relpath(path,Root)
-            List_Files.append((rel_file,M_TIME,SIZE))
+            rel_file = os.path.relpath(path, Root)
+            List_Files.append((rel_file, M_TIME, SIZE))
         if os.path.isdir(path):
-            if SubList := ScrapeFolder(path,Date,Root):
+            if SubList := ScrapeFolder(path, Date, Root):
                 for item in SubList:
                     List_Files.append(item)
     return List_Files
 
-def VerifyAction(Action:str) -> bool:
+
+def VerifyAction(Action: str) -> bool:
     """Takes in a string representing the action user wants to execute. Returns a boolean indicating
     True: Action is Print
     False: Action is Report
@@ -65,16 +69,17 @@ def VerifyAction(Action:str) -> bool:
     Returns:
         bool: True if Action is Print, False if Action is Report
     """
-    Actions = [['PRINT', 'P'],['REPORT','R']]  
+    Actions = [['PRINT', 'P'], ['REPORT', 'R']]
     if (Action.upper() in Actions[0]):
         return True
-    elif  (Action.upper() in Actions[1]):
+    elif (Action.upper() in Actions[1]):
         return False
     else:
         print(f'{Action} is not a valid action\nActions should be "Print" or "Report"')
-        raise ValueError     
+        raise ValueError
 
-def VerifyDirectory(Directory:str) -> PL.Path:
+
+def VerifyDirectory(Directory: str) -> PL.Path:
     """This function verifies a directory, it takes a string representing a path and returns a Path object.
     Raises a ValueError if the directory does not exist.
 
@@ -93,7 +98,8 @@ def VerifyDirectory(Directory:str) -> PL.Path:
         print(f'{Directory} does not exist')
         raise ValueError
 
-def VerifyDate(Date:str) -> DT.datetime:
+
+def VerifyDate(Date: str) -> DT.datetime:
     """Takes in a string representing date in ISO8601 format
 
     Args:
@@ -104,12 +110,13 @@ def VerifyDate(Date:str) -> DT.datetime:
 
     Returns:
         datetime: Returns a datetime value.
-    """    
+    """
     try:
         return DT.datetime.fromisoformat(Date)
     except ValueError:
-            print(f'{Date} is not a valid date\nDate should be in the form of YYYY-MM-DD')
-            raise ValueError
+        print(f'{Date} is not a valid date\nDate should be in the form of YYYY-MM-DD')
+        raise ValueError
+
 
 def main():
     try:
@@ -124,12 +131,13 @@ def main():
         Directory = VerifyDirectory(Directory)
         Date = VerifyDate(Date)
         Action = VerifyAction(Action)
-    except ValueError:  
-            return
+    except ValueError:
+        return
     if Action:
-        Print(Directory,Date)
+        Print(Directory, Date)
     else:
-        Report()   
+        Report()
+
 
 if __name__ == '__main__':
     main()
